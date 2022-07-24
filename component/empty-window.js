ff990338064119761a3b963a2f0b7ba132d18765
silentium.noxe <silentium.noxe@gmail.com>
2022-07-24 16:46:59 +0300

    empty window design

---
+++ component/empty-window.js
@@ -0,0 +1,54 @@
+class EmptyWindow extends HTMLElement {
+
+    static get observedAttributes() {
+        return [];
+    }
+
+    constructor() {
+        super();
+    }
+
+    #render() {
+        this.clear();
+
+        if (!this.id) {
+            this.id = generateId(32);
+        }
+
+        if (!this.classList.contains("window")) {
+            this.classList.add("window");
+        }
+
+        const $form = document.createElement("form");
+        const $input = document.createElement("input");
+        const $autocomplete = document.createElement("div");
+
+        $input.dataset.window = this.id;
+        $input.placeholder = "Enter content command";
+
+        $form.append($input);
+        $form.append($autocomplete);
+
+        this.append($form);
+    }
+
+    clear() {
+    }
+
+    connectedCallback() {
+        this.#render();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+    }
+
+    disconnectedCallback() {
+    }
+
+    adoptedCallback() {
+    }
+}
+
+if (!customElements.get("empty-window")) {
+    customElements.define("empty-window", EmptyWindow);
+}
